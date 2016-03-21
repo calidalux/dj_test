37979ff99053d99dfad6b62098fca88dee17fa71
skver <nulla.animo@gmail.com>
2016-03-21 11:17:45 +0300

    рабочие ЧПУ

--- tires/urls.py
+++ tires/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'tires'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<slug>[-\w]+)/$', views.DetailView.as_view(), name='detail'),
 ]
